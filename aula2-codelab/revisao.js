3932493072ed4bc700e97461cfca03046ede7180
alexandre-blueedtech <arussi@blueedtech.com.br>
2022-01-18 22:52:37 -0300

    codigo de blbalbal

---
+++ aula2-codelab/revisao.js
@@ -0,0 +1,50 @@
+const prompt = require("prompt-sync")();
+
+// let anos = +prompt("Digite os anos: ");
+// let meses = +prompt("Digite os meses: ");
+// let dias = +prompt("Digite os dias: ");
+
+// let anosParaDias = anos * 365;
+// let mesesParaDias = meses * 30;
+
+// let totalDias = anosParaDias + mesesParaDias + dias;
+// console.log("Total de dias q vc viveu: " + totalDias);
+
+
+// ------------
+console.log();
+// ------------
+
+// EX 04
+
+// let idadeEmDias = +prompt("Digite sua idade em dias: ");
+
+// console.log(idadeEmDias);
+
+// let diasEmAnos = Math.trunc(idadeEmDias / 365);
+// console.log("Idade em anos: " + diasEmAnos);
+
+// let restoDias1 = idadeEmDias % 365;
+
+// let diasEmMeses = Math.trunc(restoDias1 / 30);
+// console.log("Idade em meses: " + diasEmMeses);
+
+// let restoDias2 = restoDias1 % 30;
+// console.log(`Idade em dias: ${restoDias2}`);
+
+
+
+
+
+
+
+
+
+// COMECANDO PROJETO
+
+console.log(`
+SUA HISTORIA
+VC 
+ESCREVE
+AQUI
+`);
